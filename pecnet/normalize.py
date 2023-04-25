3c08d30832e21cec63e82079542551a73c643f88
SerkanMacit <codingforbetterlife@gmail.com>
2023-04-25 16:33:18 +0300

    adds input normalization window option

--- pecnet/normalize.py
+++ pecnet/normalize.py
@@ -1,5 +1,19 @@
 import numpy as np 
 
+# class WindowNormalizer():
+#     def fit(self, data):
+#         self.mean = np.mean(data, 1)
+#     def fit_transform(self, data):
+#         self.mean = np.mean(data, 1)
+#         return data - self.mean[:, np.newaxis]
+#     def transform(self, data, is_window=True):
+#         if self.mean is None:
+#             raise ValueError("Fit Before Transforming")
+#         norm = data - self.mean  
+
+#         return norm[:, np.newaxis] if is_window else norm                                                                                                                                                 
+
+
 class WindowNormalizer():
     def fit(self, data):
         self.mean = np.mean(data, 1)
@@ -9,6 +23,7 @@ class WindowNormalizer():
     def transform(self, data, is_window=True):
         if self.mean is None:
             raise ValueError("Fit Before Transforming")
-        norm = data - self.mean  
-
-        return norm[:, np.newaxis] if is_window else norm
+        if is_window:
+            return data - self.mean[:, np.newaxis]
+        else:
+            return data - self.mean
--- pecnet/preprocessing.py
+++ pecnet/preprocessing.py
@@ -1,10 +1,9 @@
-from attr import field
 from math import ceil
 import numpy as np
 from pywt import wavedec
 from pecnet.normalize import WindowNormalizer
 
-def get_rolling_windows(data, window_size, fill=False, step=1):
+def get_rolling_windows(data, window_size, fill=False, step=1, include_first=True):
     """
     Get rolling windows for the given data
      Parameters
@@ -17,15 +16,22 @@ def get_rolling_windows(data, window_size, fill=False, step=1):
     step:
         Used in the windowization process. Distance between consequent windows. 
         Ex: window_size = 4, step = 1 [1,2,3,4,5,6] -> [[1,2,3,4],[2,3,4,5], ...]
-        step = 2 [[1,2,3,4],[3,4,5,6], ...]
+        step = 2 [[1,2,3,4],[3,4,5,6], ...]            
     """
+    #TODO:  fill == False and include_first==False should not be in the same time.
+
+    margin = 0
+    if include_first == False:
+        margin = 1
+        data = data[:-1]
 
     if fill: # Fill with zeros to get exact same size
-        data = np.insert(data, np.zeros((window_size - 1), dtype=int), 0.0)
+        data = np.insert(data, np.zeros((window_size - 1 + margin), dtype=int), 0.0)
+
     windows = np.lib.stride_tricks.sliding_window_view(data, window_size)[::step, :].copy()
     return windows
-    
-def rolling_op(data, window_size, op, fill=True, step=1):
+
+def rolling_op(data, window_size, op, fill=True, step=1, include_first=True):
     """
     Get rolling operation for the given data
      Parameters
@@ -42,11 +48,11 @@ def rolling_op(data, window_size, op, fill=True, step=1):
         Ex: window_size = 4, step = 1 [1,2,3,4,5,6] -> [[1,2,3,4],[2,3,4,5], ...]
         step = 2 [[1,2,3,4],[3,4,5,6], ...]
     """
-    windows = get_rolling_windows(data, window_size, fill, step)
+    windows = get_rolling_windows(data, window_size, fill, step, include_first)
     results = op(windows, axis=-1)
     return results
 
-def rolling_mean(data, window_size, fill=True, step=1): # == yy5
+def rolling_mean(data, window_size, fill=True, step=1, include_first=True): # == yy5
     """
     Get rolling mean for the given data
      Parameters
@@ -62,7 +68,7 @@ def rolling_mean(data, window_size, fill=True, step=1): # == yy5
         Ex: window_size = 4, step = 1 [1,2,3,4,5,6] -> [[1,2,3,4],[2,3,4,5], ...]
         step = 2 [[1,2,3,4],[3,4,5,6], ...]
     """
-    windows = get_rolling_windows(data, window_size, fill, step)
+    windows = get_rolling_windows(data, window_size, fill, step, include_first)
     means = np.nanmean(windows,axis=-1)
     return means
 
@@ -93,7 +99,7 @@ def wavelet_window(window, wavelet = 'haar', level=2):
 def apply_wavelet(data, func=wavelet_window, wavelet = 'haar', level=2): #four_wavelets
     return np.apply_along_axis(func, 1, data, wavelet=wavelet, level=level)
 
-def get_xy(arr, window_size, step=1, fill=False):
+def get_xy(arr, window_size, step=1, fill=False, include_first=True):
     """
     Get X and y windowized.
      Parameters
@@ -109,7 +115,7 @@ def get_xy(arr, window_size, step=1, fill=False):
         Ex: window_size = 4, step = 1, [1,2,3,4,5,6] -> [[0,0,0,1], [0,0,1,2], ...]
 
     """
-    X = get_rolling_windows(arr, window_size, fill=fill, step=step)
+    X = get_rolling_windows(arr, window_size, fill=fill, step=step, include_first=include_first)
 
     if fill:
         y = arr[1::step]
@@ -120,7 +126,7 @@ def get_xy(arr, window_size, step=1, fill=False):
     return X, y
 
 def train_test(X,y, split_index):
-
+    
     if split_index < 0 or split_index > len(X):
         raise ValueError(f"split_index={split_index} should be in range (0, len(X) )")
     X_train = X[:split_index]
@@ -130,6 +136,14 @@ def train_test(X,y, split_index):
     y_test = y[split_index:]
 
     return X_train, X_test, y_train, y_test
+
+def label_train_test(y, split_index):
+    if split_index < 0 or split_index > len(y):
+        raise ValueError(f"split_index={split_index} should be in range (0, len(y) )")
+    y_train = y[:split_index]
+    y_test = y[split_index:]
+
+    return y_train, y_test
 
 def get_window_split_index(split_index, window_size, step, fill, start):
     """
@@ -143,7 +157,7 @@ def get_window_split_index(split_index, window_size, step, fill, start):
     return int(ceil((split_index - 1) / step)) - start if fill else int(ceil((split_index - window_size) / step) - start)
 
 
-def prepare_data(input_data, window_size, step=1, fill=False, normalize=True, start=0):
+def prepare_data(input_data, window_size, mean_window_size, step=1, fill=False, normalize=True, start=0, include_first=True):
     """
     Applies all of the preprocessing steps. 
     In Wavelet cA2 is discarded.
@@ -166,12 +180,33 @@ def prepare_data(input_data, window_size, step=1, fill=False, normalize=True, st
     ----------
     Returns X,y. If normalize, also returns mean values.
     """
-    X,y = get_xy(input_data, window_size, step=step, fill=fill)
-    scaler = None
+    # X,y = get_xy(input_data, window_size, step=step, fill=fill, include_first=include_first)
+    # scaler = None
+    # if normalize:
+    #     scaler = WindowNormalizer()
+    #     norm_X = scaler.fit_transform(X)
+    #     print("mean: ",scaler.mean)
+    #     norm_y = scaler.transform(y,False)
+    #     X = norm_X
+    #     y = norm_y
+
+    
+    X,y = get_xy(input_data, window_size, step=step, fill=fill, include_first=include_first)
+    X2,y2 = get_xy(input_data, mean_window_size, step=step, fill=fill, include_first=include_first)
     if normalize:
+        length2 = X2.shape[0]
+        scaler2 = WindowNormalizer()
+        scaler2.fit(X2)
+        norm_X2 = scaler2.transform(X[:length2])
+        norm_y2 = scaler2.transform(y[:length2],False)
+
         scaler = WindowNormalizer()
-        norm_X = scaler.fit_transform(X)
+        scaler.fit(X)
+        norm_X = scaler.transform(X)
         norm_y = scaler.transform(y,False)
+
+        norm_X[:length2] = norm_X2
+        norm_y[:length2] = norm_y2
         X = norm_X
         y = norm_y
     
@@ -182,7 +217,7 @@ def prepare_data(input_data, window_size, step=1, fill=False, normalize=True, st
 
     return X[start:], y[start:]
 
-def prepare_error_data(input_data, window_size, step=1, fill=False, normalize=True, start=0):
+def prepare_error_data(input_data, window_size, mean_window_size, step=1, fill=False, normalize=True, start=0, include_first=True):
     """
     Almost same as prepare_data. Only difference is last value is clipped. So there is no nan value in the end of the output.
     In Wavelet cA2 is discarded.
@@ -205,12 +240,31 @@ def prepare_error_data(input_data, window_size, step=1, fill=False, normalize=Tr
     ----------
     Returns X,y. If normalize, also returns mean values.
     """
-    X,y = get_xy(input_data, window_size, step=step, fill=fill)
-    scaler = None
+    # X,y = get_xy(input_data, window_size, step=step, fill=fill)
+    # scaler = None
+    # if normalize:
+    #     scaler = WindowNormalizer()
+    #     norm_X = scaler.fit_transform(X)
+    #     norm_y = scaler.transform(y,False)
+    #     X = norm_X
+    #     y = norm_y
+    
+    X,y = get_xy(input_data, window_size, step=step, fill=fill, include_first=include_first)
+    X2,y2 = get_xy(input_data, mean_window_size, step=step, fill=fill, include_first=include_first)
     if normalize:
+        length2 = X2.shape[0]
+        scaler2 = WindowNormalizer()
+        scaler2.fit(X2)
+        norm_X2 = scaler2.transform(X[:length2])
+        norm_y2 = scaler2.transform(y[:length2],False)
+
         scaler = WindowNormalizer()
-        norm_X = scaler.fit_transform(X)
+        scaler.fit(X)
+        norm_X = scaler.transform(X)
         norm_y = scaler.transform(y,False)
+
+        norm_X[:length2] = norm_X2
+        norm_y[:length2] = norm_y2
         X = norm_X
         y = norm_y
     
@@ -221,7 +275,7 @@ def prepare_error_data(input_data, window_size, step=1, fill=False, normalize=Tr
 
     return X[start:-1], y[start:-1]
 
-def prepare_and_split_data(input_data, window_size, split_index=None, step=1, fill=False, normalize=True, start=0, window_split_index=None):
+def prepare_and_split_data(input_data, window_size, mean_window_size, split_index=None, step=1, fill=False, normalize=True, start=0, window_split_index=None, include_first=True):
     """
     Applies all of the preprocessing steps. 
     In Wavelet cA2 is discarded.
@@ -254,14 +308,14 @@ def prepare_and_split_data(input_data, window_size, split_index=None, step=1, fi
         window_split_index = get_window_split_index(split_index, window_size, step, fill, start)
 
     if normalize:
-        X, y, mean = prepare_data(input_data, window_size, step, fill, normalize, start)
+        X, y, mean = prepare_data(input_data, window_size, mean_window_size, step, fill, normalize, start, include_first)
         return *train_test(X, y, window_split_index), mean
 
-    X, y = prepare_data(input_data, window_size, step, fill, normalize, start)
+    X, y = prepare_data(input_data, window_size, mean_window_size, step, fill, normalize, start, include_first)
     return train_test(X, y, window_split_index)
 
 
-def prepare_and_split_error_data(input_data, window_size, split_index=None, step=1, fill=False, normalize=True, start=0, window_split_index=None):
+def prepare_and_split_error_data(input_data, window_size, mean_window_size, split_index=None, step=1, fill=False, normalize=True, start=0, window_split_index=None):
     """
     Applies all of the preprocessing steps. 
     In Wavelet cA2 is discarded.
@@ -294,8 +348,8 @@ def prepare_and_split_error_data(input_data, window_size, split_index=None, step
         window_split_index = get_window_split_index(split_index, window_size, step, fill, start)
 
     if normalize:
-        X, y, mean = prepare_error_data(input_data, window_size, step, fill, normalize, start)
+        X, y, mean = prepare_error_data(input_data, window_size, mean_window_size, step, fill, normalize, start)
         return *train_test(X, y, window_split_index), mean
 
-    X, y = prepare_error_data(input_data, window_size, step, fill, normalize, start)
+    X, y = prepare_error_data(input_data, window_size, mean_window_size, step, fill, normalize, start)
     return train_test(X, y, window_split_index)
